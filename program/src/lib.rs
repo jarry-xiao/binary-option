ac8cce10e3bde252ac7e3385334c85119ec9a8a6
jarry-xiao <jarry.xiao@gmail.com>
2021-07-09 17:24:44 -0500

    restructured directory

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -9,4 +9,4 @@ pub mod validation_utils;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("5UBdr5zwP8kN54xeTRZ6LyAUqLyRMMuP77v3r3jQowMr");
+solana_program::declare_id!("betw959P4WToez4DkuXwNsJszqbpe3HuY56AcG5yevx");
